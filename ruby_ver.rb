8500f79eae424b576f78aa55a05caba1ca129644
Dylan Perry <dylanbperry@gmail.com>
2024-06-12 16:48:47 -0700

    Add method (check_word(word)) to return all possible anagrams of inputted word. Method is hardcoded to utilize lib/words_alpha.txt

--- ruby_ver.rb
+++ ruby_ver.rb
@@ -1,12 +1,53 @@
 # require 'lib/words_alpha.txt'
 
-def initialize_dictionary_file(file_path)
-    @words_array = File.readlines(file_path).map(&:strip)
+# def initialize_dictionary_file(file_path)
+#     @dict_array = File.readlines(file_path).map(&:strip)
+# end
+
+# def initialize_word_details(word)
+#     @input_word_chars = word.chars
+#     @word_chars_count = word.chars.count
+# end
+
+def create_letter_hash(input_word)
+    input_word.chars.group_by(&:itself).map { | letter, array | [ letter, array.length ] }.to_h
 end
 
-def initialize_word_details(word)
-    @word_chars = word.chars
-    @word_chars_count = word.chars.count
+def word_possible?(input, reference)
+    input_word_hash = create_letter_hash(input)
+    reference_word_hash = create_letter_hash(reference)
+
+    reference_word_hash.each do |letter, remaining_instances|
+        # Checking if each letter from the reference is present in the input. If not, reference word not possible.
+        unless (input_word_hash.key?(letter))
+            return false
+        end
+
+        # Subtracting a usage of the letter from input
+        input_word_hash[letter] -= remaining_instances
+    
+        # Checking if there were any instances of the input letter remaining. If not, reference word not possible.
+        if (input_word_hash[letter] < 0)
+            return false
+        end
+    end
+    
+    # Returning true, since checks ensure reference word is possible.
+    true
+end
+
+def check_word(input_word)
+    @dict_array = File.readlines('lib/words_alpha.txt').map(&:strip)
+
+    @matching_words_array = []
+
+    @dict_array.map do |dict_word|
+        if word_possible?(input_word, dict_word)
+            @matching_words_array << dict_word
+        end
+    end
+
+    @matching_words_array
 end
 
 require 'pry'; binding.pry
