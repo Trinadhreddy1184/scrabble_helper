4bbcef7c87347655de704a09043b545c7f7864ec
Dylan Perry <dylanbperry@gmail.com>
2024-06-20 11:46:11 -0700

    Add function get_word_list to return a list of words constructible via user inputted word

--- elixir_ver.exs
+++ elixir_ver.exs
@@ -1,4 +1,5 @@
 defmodule ScrabbleModule do
+  ## Function to convert word into a map of letter => letter instances
   def mapify_word(word) do
     word
     |> String.downcase()
@@ -8,11 +9,10 @@ defmodule ScrabbleModule do
     |> Map.new()
   end
 
+  ## Boolean function to check whether a reference word can be made from the characters of an inputted word
   def word_possible?(input, reference) do
     input_map = mapify_word(input)
     reference_map = mapify_word(reference)
-
-    input_keys = Map.keys(input_map)
     reference_keys = Map.keys(reference_map)
 
     ## 'Cond' implementation, checking that each reference letter count >= each input letter count, and also no letter counts are nil
@@ -24,26 +24,14 @@ defmodule ScrabbleModule do
       true ->
         true
     end
+  end
 
-    ## 'If' implementation, using above cond logic
-    # if !Enum.all?(reference_keys, fn key -> Map.get(input_map, key) >= Map.get(reference_map, key) && Map.get(input_map, key) != nil end) do
-    #   false
-    # else
-    #   true
-    # end
-
-    ## 'Nested Cond' implementation, separates key check and letter count check into separate nested conds
-    # cond do
-    #   !Enum.all?(reference_keys, fn key -> Map.has_key?(input_map, key) end) ->
-    #     false
-    #   Enum.all?(reference_keys, fn key -> Map.has_key?(input_map, key) end) ->
-    #     cond do
-    #       !Enum.all?(reference_keys, fn key -> Map.get(input_map, key) >= Map.get(reference_map, key) end) ->
-    #         false
-    #     true ->
-    #       true
-    #     end
-    # end
+  ## Returns a list of dictionary words passing the word_possible? check
+  def get_word_list(input_word) do
+    File.stream!("lib/words_alpha.txt", [:trim_bom, encoding: :utf8])
+    |> Stream.map(&String.trim/1)
+    |> Enum.to_list()
+    |> Enum.filter(fn ref_word -> word_possible?(input_word, ref_word) end)
   end
 end
 
