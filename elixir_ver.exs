0c89cc7894313a42872a232d5333eed76795535d
Dylan Perry <dylanbperry@gmail.com>
2024-06-19 23:52:04 -0700

    Add function word_possible? to check validity of inputted word against dictionary/reference word

--- elixir_ver.exs
+++ elixir_ver.exs
@@ -1,11 +1,48 @@
 defmodule ScrabbleModule do
-  def process_word_for_hashing(word) do
+  def mapify_word(word) do
     word_downcased = String.downcase(word)
     word_char_list = String.graphemes(word_downcased)
     grouped_char_list = Enum.group_by(word_char_list, fn x -> x end)
     char_tuple_list = Enum.map(grouped_char_list, fn {char, char_list} -> {char, length(char_list)} end)
-    char_map = Map.new(char_tuple_list)
-    char_map
+    Map.new(char_tuple_list)
+  end
+
+  def word_possible?(input, reference) do
+    input_map = mapify_word(input)
+    reference_map = mapify_word(reference)
+
+    input_keys = Map.keys(input_map)
+    reference_keys = Map.keys(reference_map)
+
+    ## 'Cond' implementation, checking that each reference letter count >= each input letter count, and also no letter counts are nil
+    cond do
+      !Enum.all?(reference_keys, fn key ->
+        Map.get(input_map, key) >= Map.get(reference_map, key) &&
+        Map.get(input_map, key) != nil end) ->
+          false
+      true ->
+        true
+    end
+
+    ## 'If' implementation, using above cond logic
+    # if !Enum.all?(reference_keys, fn key -> Map.get(input_map, key) >= Map.get(reference_map, key) && Map.get(input_map, key) != nil end) do
+    #   false
+    # else
+    #   true
+    # end
+
+    ## 'Nested Cond' implementation, separates key check and letter count check into separate nested conds
+    # cond do
+    #   !Enum.all?(reference_keys, fn key -> Map.has_key?(input_map, key) end) ->
+    #     false
+    #   Enum.all?(reference_keys, fn key -> Map.has_key?(input_map, key) end) ->
+    #     cond do
+    #       !Enum.all?(reference_keys, fn key -> Map.get(input_map, key) >= Map.get(reference_map, key) end) ->
+    #         false
+    #     true ->
+    #       true
+    #     end
+    # end
   end
 end
 
