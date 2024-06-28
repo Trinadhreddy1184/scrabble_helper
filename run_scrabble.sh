4fc5df8f662b6c1a2a6af1d2285e96c53d7654dd
Trinadhreddy Seelam <trinadhreddysee@gmail.com>
2024-06-28 11:15:32 -0400

    Update run_scrabble.sh

--- run_scrabble.sh
+++ run_scrabble.sh
@@ -2,8 +2,9 @@ echo "Welcome to the Scrabble Helper!"
 echo "Which version would you like to run?"
 echo "1) Ruby Version"
 echo "2) Elixir Version"
+echo "3) Python Version"
 
-read -p "Enter a version (1 or 2): " selection
+read -p "Enter a version (1 or 2 or 3): " selection
 
 if [ "$selection" == "1" ]; then
   echo "Running Ruby version..."
@@ -11,6 +12,9 @@ if [ "$selection" == "1" ]; then
 elif [ "$selection" == "2" ]; then
   echo "Running Elixir version..."
   elixir bin/elixir_ver.exs
+elif [ "$selection" == "3" ]; then
+  echo "Running Python version..."
+  pyhton bin/python_ver.py  
 else
-  echo "Invalid choice. Please enter 1 or 2."
+  echo "Invalid choice. Please enter 1 or 2 or 3."
 fi
